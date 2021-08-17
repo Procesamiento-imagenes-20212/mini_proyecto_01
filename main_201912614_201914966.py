535307279ed82baac139cdcd09b990a134964579
gomez2608 <s.gomeza1@uniandes.edu.co>
2021-08-17 17:36:59 -0500

    Librerias importadas y anotaciones importantes

--- main_201912614_201914966.py
+++ main_201912614_201914966.py
@@ -1,1 +1,8 @@
 print("Hello world")
+#No desplegar imágenes simultaneamente a menos que se esté contrastando, usar 
+#Generar rutas usando os.path.join ej: "skimage.io.imread(os.path.join('ims','im.png))"
+import os
+import shutil
+import glob
+import cv2
+import nilabel
